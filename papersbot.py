c1c6d2231f2fa6d333d3a1e974a859a5a8870d20
yuhchyuanchang <yuhchyuan.chang@atomis.co.jp>
2026-05-27 16:15:48 +0900

    Update papersbot.py

--- papersbot.py
+++ papersbot.py
@@ -462,7 +462,7 @@ def main():
 
     # Initialize our bot
     doTweet = "--do-not-tweet" not in sys.argv
-    bot = PapersBot(False)
+    bot = PapersBot(doTweet)
 
     # We can print top tweets
     if "--top-tweets" in sys.argv:
